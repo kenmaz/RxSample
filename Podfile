e5f63658f8d23d5321e527dd517ab2dcb8aaf1f6
Kentaro Matsumae <kentaro.matsumae@dena.com>
2018-03-23 02:25:50 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, "11"
+use_frameworks!
+
+target "RxSample" do
+  pod 'RxSwift'
+  pod 'RxCocoa'
+end
--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var pageLabel: UILabel!
+    @IBOutlet weak var fowardButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
+    var viewModel = ViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prevButton.rx.tap.asObservable().subscribe(onNext: { [weak self] () in
+            self?.viewModel.prevButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        fowardButton.rx.tap.asObservable().subscribe(onNext: { [weak self] () in
+            self?.viewModel.forwardButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        addButton.rx.tap.asObservable().subscribe(onNext: {[weak self] () in
+            self?.viewModel.addButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        viewModel.label.asObservable().subscribe(onNext: {[weak self] (label) in
+            self?.pageLabel.text = label
+        }).disposed(by: disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    class ViewModel {
+        struct Page {
+            let idx: Int
+        }
+        var pages: BehaviorRelay<[Page]> = BehaviorRelay(value: [])
+        var index: BehaviorRelay<Int> = BehaviorRelay(value: 0)
+        var label: BehaviorRelay<String> = BehaviorRelay(value: "")
+        
+        init() {
+            _ = pages.subscribe(onNext: {[weak self] (pages) in
+                self?.updateLabel()
+            })
+            _ = index.subscribe(onNext: {[weak self] (_) in
+                self?.updateLabel()
+            })
+            addPage()
+        }
+        
+        private func updateLabel() {
+            self.label.accept("\(index.value + 1)/\(pages.value.count)")
+        }
+        
+        func prevButtonDidTap() {
+            if index.value > 0 {
+                index.accept(index.value - 1)
+            } else {
+                index.accept(pages.value.count - 1)
+            }
+        }
+        func forwardButtonDidTap() {
+            if index.value < pages.value.count - 1 {
+                index.accept(index.value + 1)
+            } else {
+                index.accept(0)
+            }
+        }
+        func addButtonDidTap() {
+            addPage()
+        }
+        private func addPage() {
+            var list = pages.value
+            let idx = list.count + 1
+            let page = Page(idx: idx)
+            list.append(page)
+            pages.accept(list)
+        }
+        
     }
-
-
 }
 
